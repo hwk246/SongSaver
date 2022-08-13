825825befe7ce783eb70399801b6707767a01a6f
Ron Klinker <r.klinker@gmail.com>
2022-08-13 09:45:14 +0200

    test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SongOverview from "./components/SongOverview";
+import TestComponent from "./components/TestComponent";
 
 function App() {
   return (
     <div>
       <SongOverview />
+      <TestComponent />
     </div>
   );
 }
